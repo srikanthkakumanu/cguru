c0439631f4b6ebdc14be71a97c6ddfe1a2c1c327
Srikanth Kakumanu <srikanthkakumanu@yahoo.com>
2021-04-02 01:53:47 +0530

    Included sdkman script

---
+++ demo/provisioning/ansible/roles/commons/files/sdkman-jdk-setup.sh
@@ -0,0 +1,5 @@
+curl -s "https://get.sdkman.io" | bash
+source "/home/srikanth/.sdkman/bin/sdkman-init.sh"
+sdk install java 8.0.272.hs-adpt
+sdk use java 8.0.272.hs-adpt
+sdk default java 8.0.272.hs-adpt
