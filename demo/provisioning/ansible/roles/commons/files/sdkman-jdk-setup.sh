10edbe1c5178e196466dcdde4d467c09d30018a0
Srikanth Kakumanu <srikanthkakumanu@yahoo.com>
2021-04-12 00:48:50 +0530

    componentized ngrok as separate role

--- demo/provisioning/ansible/roles/commons/files/sdkman-jdk-setup.sh
+++
@@ -1,5 +0,0 @@
-curl -s "https://get.sdkman.io" | bash
-source "/home/srikanth/.sdkman/bin/sdkman-init.sh"
-sdk install java 8.0.272.hs-adpt
-sdk use java 8.0.272.hs-adpt
-sdk default java 8.0.272.hs-adpt
